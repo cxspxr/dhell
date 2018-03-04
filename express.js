d6b9e8da4b4d94d78acd6c3b717e2d4c61975bf3
casper <ycasperinho@gmail.com>
2018-03-05 00:07:21 +0200

    set up paths to send and receive encrypted keys

--- express.js
+++ express.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const env = require('dotenv');
 
+app.get('/send/:encryptedKey', function(req, res) {
 
+});
+
+app.get('/receive/:encryptedKey', function(req, res) {
+
+});
 
 app.listen(3000, function() {
     console.log('listening 3000');
